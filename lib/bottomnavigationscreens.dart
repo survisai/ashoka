db0df8bd48fee24b2fdbde8008958db3ec970ee7
survisai <survisai66@gmail.com>
2021-10-25 12:23:14 +0530

    firebase

--- lib/bottomnavigationscreens.dart
+++ lib/bottomnavigationscreens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Screen1 extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:ashoka/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-
-void main() => runApp(MyApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This is the root widget of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Ashoka',
-
-        home: Splash()
-    );
+        debugShowCheckedModeBanner: false, title: 'Ashoka', home: Splash());
   }
 }
 //=====================================================================
